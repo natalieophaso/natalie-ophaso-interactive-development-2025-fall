8377a60d9f6738ccc7b53913de00e3ba1ee27746
natalieophaso <natalieophaso@gmail.com>
2025-12-13 05:06:02 -0600

    Third Page

---
+++ harmonic-collection/script-museum4.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    $("div").animate({ 
+      height: '17rem',
+      width: '1rem',
+      padding: '1rem 1rem'
+
+    });
+  });
+});
